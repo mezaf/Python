d54e8cf11147786595632bacd77824ba2a5f2b91
mezaf <mezap.fernando@gmail.com>
2019-04-07 21:53:14 -0300

    update json keys

--- json_keys.py
+++ json_keys.py
@@ -1,15 +1,16 @@
 import json
-def upper_flat_json(obj):
+def upper_flat_json(obj,keys=[]):
     def flat_json(obj, new_obj={}, keys=[]):
         for key, value in obj.items():
             if isinstance(value, dict):
                 new_obj = flat_json(obj[key], new_obj, keys + [key])
             else:
                 new_obj['_'.join(keys + [key])] = value
+        print(new_obj)
         return new_obj
     return flat_json(obj)
 
-def check_json(obj,json_list=[]):
+def check_json(obj,json_list=[], data=[]):
     try:
         data = json.loads(obj)
         if isinstance(data,dict):
@@ -17,27 +18,34 @@ def check_json(obj,json_list=[]):
         else:
             data = multiple_json(data)
     except:
-        # print(json_list)
         for i in json_list:
-            print(i)
+            data = 'error'
     return data 
 
 def multiple_json(obj,json_list=[]):
     for data in obj:
-        # json_list[obj.index(data)] = flat_json(data)
-        # print(obj)
         json_list.append(upper_flat_json(data).copy())
         data = json.loads(json.dumps(json_list.pop()))
         for k,v in data.items():
-            try:
-                multiple_json(json.loads(v),json_list)
-            except:
-                if isinstance(v,list):
-                    for i in v:
-                        data[k] = i
-                        json_list.append(data.copy())
+            if isinstance(v, list):
+                # print('is a list')
+                for i in v:
+                    # print(i)
+                    data[k] = i
+                    json_list.append(data.copy())
     return json_list
 
 with open('data.json', 'r') as file:
-    # print(flat_keys(file.read()))
-    print(check_json(file.read()))
+    i = True
+    data = (check_json(file.read()))
+    while i:
+        i = False
+        for data_set in data:
+            for k,v in data_set.items():
+                if isinstance(v, (list,dict)):
+                    data = check_json(json.dumps(data))
+                    i = True
+                    break
+                # elif isinstance(v, dict):
+                #     data = multiple_json(data)
+                #     i = True
