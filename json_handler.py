17eb45829fbf4c73e1c0ff67e457f5af16b8206a
mezaf <mezap.fernando@gmail.com>
2019-04-19 16:45:40 -0300

    json handler final

--- json_handler.py
+++ json_handler.py
@@ -1,47 +1,60 @@
 import json
+from copy import deepcopy
+
 
 def upper_flat_json(obj,keys=[]):
-    def flat_json(obj, new_obj={}, keys=[]):
-        for key, value in obj.items():
-            if isinstance(value, dict):
-                new_obj = flat_json(obj[key], new_obj, keys + [key])
-            else:
-                new_obj['_'.join(keys + [key])] = value
-        return new_obj
-    return flat_json(obj)
+	def flat_json(obj, new_obj={}, keys=[]):
+		for key, value in obj.items():
+			if isinstance(value, dict):
+				new_obj = flat_json(obj[key], new_obj, keys + [key])
+			else:
+				new_obj['_'.join(keys + [key])] = value
+		return new_obj
+	return flat_json(obj)
 
-def handle_list(obj,new_obj = []):
-	for data in range(len(obj)):
-		data = obj.pop()
-		if isinstance(data,list):
-			handle_list(data,new_obj)
-		else:
-			new_obj.append(data)
-	return(new_obj)
+def read_json(file=None,json_new_obj=[]):
+	if file:
+		try:
+			obj = json.loads(file.read())
+		except:
+			obj = json.loads(json.dumps(file.read()))
+		data = validation(obj)
+	else:
+		return 'No JSON provided'
+	return data
 
+def handle_list(list_obj,list_new_obj=[]):
+	for key, value in list_obj.items():
+		if isinstance(value, list):
+			for idx,val in enumerate(value):
+				list_obj[key] = val
+				list_new_obj.append(list_obj.copy())
+			break
+		elif isinstance(value, dict):
+			list_new_obj.append(list_obj)
+	return list_new_obj
 
-with open('data.json', 'r') as file:
-	new_obj = []
-	obj = json.loads(file.read())
+def validation(val_obj,final_obj=[],types=[]):
 	i = True
 	while i:
 		i = False
-		if isinstance(obj,list):
-			for _ in range(len(obj)):
-				data = obj.pop()
-				for k,v in data.items():
-					if isinstance(v,list):
-						i = True
-						for _ in range(len(v)):
-							data[k] = v.pop()
-							new_obj.append(data.copy())
-							print(obj)
-						break
-					elif isinstance(v,dict):
-						new_obj.append(upper_flat_json(data).copy())
-		else: 
-			new_obj.append(data.copy())
-		obj = new_obj
-		print(new_obj,obj)
-	# obj = handle_list(obj)
-	print(obj)
+		alpha=0
+		data = val_obj.pop(0)
+		for key, value in data.items():
+			if isinstance(value, dict):
+				val_obj.append(upper_flat_json(data).copy())
+				data = val_obj.pop(-1)
+				i=True
+				break
+		for key, value in data.items():
+			alpha += 1
+			if isinstance(value,list):
+				val_obj.extend(handle_list(data,[]))
+				i = True
+				break
+			elif alpha == len(data):
+				val_obj.append(data.copy())
+	return(val_obj)
+
+with open('data.json', 'r') as file:
+	print(read_json(file))
