2d031a36d95153fa1e12c77dabbc5b268b415b4c
Fernando Meza <43585972+mezaf@users.noreply.github.com>
2019-08-16 16:17:19 -0300

    Add files via upload

--- clean_nifi_queues.py
+++ clean_nifi_queues.py
@@ -1,4 +1,5 @@
-import json, requests
+import json
+import urllib.request as req
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
             ,'Content-Type':'application/json'
@@ -8,19 +9,19 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
             ,'Referer':'http://phoenix.paraport.com:1055/swagger/ui/index'
             }
 get_groups = 'http://localhost:8080/nifi-api/flow/process-groups/96649e4d-016c-1000-4c04-297507e11e44'
-data = {
-    "listingRequest": {"id":"9678fe1d-016c-1000-aac2-7eec798158d1"}
-}
-r = requests.get(url=get_groups)
-groups = json.loads(r.text)  
+
+group_request = req.Request(get_groups,method='GET')
+groups = json.loads(req.urlopen(group_request).read())
+
 ids = [i['id'] for i in groups['processGroupFlow']['flow']['processGroups']]
 
 get_connections = 'http://localhost:8080/nifi-api/process-groups/{id}/connections'
 connections = []
 for i in ids:
-    request_connections = json.loads(requests.get(url=get_connections.format(id=i)).text)
+    connection_request = req.Request(get_connections.format(id=i),method='GET')
+    request_connections = json.loads(req.urlopen(connection_request).read())
     for j in request_connections['connections']:
         connections.append(j['id'])
 
 delete_queues = 'http://localhost:8080/nifi-api/flowfile-queues/{id}/drop-requests'
-[requests.post(url=delete_queues.format(id=i)) for i in connections]
+[req.urlopen(req.Request(url=delete_queues.format(id=i),method='POST')).read() for i in connections]
